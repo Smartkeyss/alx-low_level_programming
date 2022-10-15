b2de364bb0a538a08c83e1ffc09bdd2ed6627b73
Smartkeyss <smartrhoda95@gmail.com>
2022-10-15 08:20:50 -0700

    task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main -> Input the ASCII number corresponding digits
+ * print output
+ * Return: always (0) success
+ */
+
+int main (void)
+
+{
+	int x;
+
+	for (x == 0; x < 10; x++)
+	{
+		putchar(x);
+	}
+	putchar(10);
+	return (0);
+}
+
