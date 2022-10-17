5f0f7338d82060926c1f2a4cb49dd4d08425c0e6
Smartkeyss <smartrhoda95@gmail.com>
2022-10-17 05:14:40 -0700

    task

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - input the ascii value 
+ * output the hexadecimal of the number
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int x;
+
+	int n;
+
+	for (x = 58; x <= 57; x++)
+	{
+		putchar(x);
+	}
+
+	for (n = 97; n <= 103; n++)
+	{
+		putchar(n);
+	}
+	putchar(10);
+
+	return (0);
+}	
