b56e6e8c454df8ded144f03f558995e444810500
Smartkeyss <smartrhoda95@gmail.com>
2022-10-17 01:30:21 -0700

    task

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
+
+
 /**
- * main - input ASCII number that is related to the character
- * 
- * return: Always 0 (success)
+ * main -> assign a random number to the variable n each time it is executed
+ * and print the last digit of the number stored in the variable n
+ * Return: Always 0 (Success)
  */
 
 int main(void)
-{
 
+{
 	int ch;
 
 	for (ch = 97; ch <= 122; ch++)
@@ -19,7 +21,6 @@ int main(void)
 	{
 		putchar(ch);
 	}
-	putchar(10);
-
+	putchar(10); /* this is an ascii code for new line*/
 	return (0);
 }
